78b82e2f0590abac1b4b92e15286221f5128e2a0
Piotr Sus <suotr12@gmail.com>
2025-12-01 21:44:46 +0100

    First-task-of-DAY-1

---
+++ day1/task1.py
@@ -0,0 +1,39 @@
+f = open("input.txt")
+data = []
+
+RED = '\033[91m'   # Jasnoczerwony
+END = '\033[0m'    # Resetowanie koloru
+
+for i in f:
+    data.append(i.rstrip().split())
+
+startPosition = 50
+zeroPointer = 0
+
+currentPosition = startPosition
+
+for move in data:
+    if move[0][0] == 'R':
+        addition = int(move[0][1:])%100
+        currentPosition += addition
+        if currentPosition > 99:
+            currentPosition = currentPosition - 100
+        print("Current addition = ", addition, '\n' )
+        print("Current pos: ", currentPosition, '\n')
+    else:
+        subtraction = int(move[0][1:])%100
+        currentPosition -= subtraction
+        if currentPosition < 0:
+            currentPosition = currentPosition + 100
+        print("Current subtraction = ", subtraction, '\n' )
+        print("Current pos: ", currentPosition, '\n')
+    
+    if currentPosition == 0:
+        zeroPointer+=1
+        print(f"{RED} ZERO FOUND!!! {END}")
+
+
+print("Zero occured ", zeroPointer , " times.")
+
+
+
